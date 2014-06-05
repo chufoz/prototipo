380707cce929ef7f0d16939c94578a6e3e6bb5cd
José Enrique García García <egarcia@estadias2.ictc.com.mx>
2014-06-05 16:00:55 -0500

    implementacion de calendario

--- utilerias/js/widgetcalendario.js
+++ utilerias/js/widgetcalendario.js
@@ -1,5 +1,12 @@
-$(function(){
-$('.wcalendar').datepicker({
-	format:'yyyy-mm-dd H:mm:ss P'
-});//funcion del datapicker
+$( document ).ready(function() {
+$('.Wcalendar').datetimepicker({
+    language:  'es',
+    weekStart: 1,
+    todayBtn:  1,
+	autoclose: 1,
+	todayHighlight: 1,
+	startView: 2,
+	forceParse: 0,
+    showMeridian: 1
+});
 });
